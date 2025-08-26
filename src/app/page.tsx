4b860a22e6e7674d376bb7dd626669871a8c999a
cyclespace <stackecommerce@gmail.com>
2025-08-26 19:11:46 +0200

    about us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Hero } from "@/components/home/hero";
 import techAnimalsBg from '@/assets/tech-animals-blue.png';
 import React from "react";
+import { AboutUs } from "@/components/home/about-us";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Hero
-        title="UNLOCKING DIGITAL SUCCESS"
-        description="TECH SOLUTIONS WITHOUT THE HASSLE"
-        backgroundImage={techAnimalsBg}
-      />
+      <div className="min-h-screen">
+        <Hero
+          title="UNLOCKING DIGITAL SUCCESS"
+          description="TECH SOLUTIONS WITHOUT THE HASSLE"
+          backgroundImage={techAnimalsBg}
+        />
+        <AboutUs />
+      </div>
     </main>
   );
 }
